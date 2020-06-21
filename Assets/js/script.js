8595e176dfe355d59603ce484d0ea2729629d660
Taryn Fanning <taryn.fanning@gmail.com>
2020-06-20 20:12:30 -0500

    questions in java as arrays

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,20 @@
-var questions = [{
-    title: "Commonly used data types DO NOT include:", options: ["strings", "booleans", "alerts", "numbers"],
+var questions = [
+    {title: "Commonly used data types DO NOT include:", options: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"},
     {title: "The condition in an if / else statement is enclosed with _____.",
     options: ["quotes", "cutly brackets", "parenthesis", "square brackets"], 
-    answer: "parenthesis"
-},
+    answer: "parenthesis"},
+    {title: "Arrays in JavaScript can be used to store _____.", 
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"},
+    {title: "String values must be enclosed within _____ when being assigned to variables",
+    options: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"},
+    {title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"},
+]
 
-]                             
 var questionsSection = document.getElementById("questions")
 var startButton = document.getElementById("btn")
 var questionIndex = 0
@@ -29,14 +37,16 @@ function getQuestions() {
         choiceEl.setAttribute("value", option)
         choiceEl.textContent = option
         questionsSection.appendChild(choiceEl)
+        displayOptions.appendChild(choiceEl)
         choiceEl.onclick=changeQuestions
     })
 }
 
+
 function changeQuestions() {
     questionIndex++
     getQuestions()
-    if (questions[questionIndex].value == answer) {
+    if (choiceEl===questions.answer) {
         alert("correct!")
     }
     else {alert("wrong")}
